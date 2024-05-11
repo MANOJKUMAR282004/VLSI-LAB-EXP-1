f4901ba0d05702ba1533958550e72e75a44d1be6
MANOJKUMAR282004 <167055290+MANOJKUMAR282004@users.noreply.github.com>
2024-05-11 13:47:25 +0530

    Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,3 +1,12 @@
+
+module halfsubtractor( D,Bo,A,B);
+input A,B;
+output D,Bo;
+wire w1;
+xor (D,A,B);
+not (w1,B);
+and (Bo,B,w1);
+endmodule
 module hs(a,b,difference,borrow);
 input a,b;
 output difference,borrow;
